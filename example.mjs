2d3562d59bce7979e13e743b94fb8e1a3a17d1d9
H Degroote <8046565-HDegroote@users.noreply.gitlab.com>
2023-05-31 21:09:00 +0000

    Add example

---
+++ example.mjs
@@ -0,0 +1,62 @@
+import Corestore from 'corestore'
+import ram from 'random-access-memory'
+import Hyperswarm from 'hyperswarm'
+import SwarmManager from './index.js'
+
+const store = new Corestore(ram)
+
+const swarm = new Hyperswarm()
+swarm.on('connection', (socket) => {
+  store.replicate(socket)
+  socket.on('error', () => {})
+})
+
+const manager = new SwarmManager(swarm)
+
+// Some dummy discovery keys
+const keys = [
+  'a'.repeat(64),
+  'b'.repeat(64)
+]
+
+manager.serve(keys[0])
+manager.serve(keys[1])
+manager.request(keys[1])
+manager.serve(keys[1]) // served twice
+
+console.log('Served keys:')
+printKeys(manager.servedKeys)
+
+console.log('requested keys:')
+printKeys(manager.requestedKeys)
+
+await manager.unserve(keys[1]) // still served once
+console.log('\nServed keys after one unserve:')
+printKeys(manager.servedKeys)
+
+console.log('Requested keys after one unserve:')
+printKeys(manager.requestedKeys)
+
+await manager.unserve(keys[1]) // no longer served--still requested
+console.log('\nServed keys after second unserve:')
+printKeys(manager.servedKeys)
+
+console.log('Requested keys after second unserve:')
+printKeys(manager.requestedKeys)
+
+await manager.unrequest(keys[1]) // no longer requested
+console.log('\nFinal served keys:')
+printKeys(manager.servedKeys)
+
+console.log('Final requested keys:')
+printKeys(manager.requestedKeys)
+
+await manager.close()
+
+function printKeys (keys) {
+  if (keys.length === 0) {
+    console.log('/')
+  } else {
+    console.log(` - ${keys.map(key => key.toString('hex')).join('\n - ')}`)
+  }
+}
